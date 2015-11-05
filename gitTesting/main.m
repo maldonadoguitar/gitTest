9daa54ca3d83e8a265555f62453dd1f27201724e
Christian <maldonadoguitar@gmail.com>
2015-11-05 12:30:02 -0500

    test1

---
+++ gitTesting/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitTesting
+//
+//  Created by Christian Maldonado on 11/5/15.
+//  Copyright © 2015 Christian Maldonado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
